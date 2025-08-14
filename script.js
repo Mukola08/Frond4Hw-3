c70c0ff9a6c3cad8cb920bd7ab9f76ffd0a489ca
Mukola08 <163895904+Mukola08@users.noreply.github.com>
2025-08-14 11:35:20 +0300

    full

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const imgObServer = new IntersectionObserver((entris, observer) => {
         if (elem.isIntersecting) {
             elem.target.src = elem.target.dataset.src;
             elem.target.style.transform = "rotate(0deg)"
+            observer.unobserve(elem.target)
         }
     }) 
 })
